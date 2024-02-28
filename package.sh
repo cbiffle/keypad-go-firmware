b5fccd7b09f5c2ce9355b1a630089081d3336eb2
Cliff L. Biffle <code@cliffle.com>
2024-02-28 08:48:02 -0800

    Add a README.

--- package.sh
+++ package.sh
@@ -14,11 +14,13 @@ arm-none-eabi-objcopy -Obinary $OUT/keybad-fw $OUT/C011xx.bin
 cp $OUT/C011xx.elf $OUT/C031xx.elf
 cp $OUT/C011xx.bin $OUT/C031xx.bin
 
-cargo metadata --format-version 1 \
-    | jq -r '.resolve.root as $root | .packages[] | select(.id == $root) | .version' \
-    > $OUT/VERSION
+VERSION=$(cargo metadata --format-version 1 \
+    | jq -r '.resolve.root as $root | .packages[] | select(.id == $root) | .version')
+echo $VERSION > $OUT/VERSION
 
 cd $OUT
 zip firmware.zip VERSION \
     C031xx.bin C011xx.bin G03xxx_G04xxx.bin \
     C031xx.elf C011xx.elf G03xxx_G04xxx.elf
+cd -
+mv $OUT/firmware.zip keypad-go-firmware-$VERSION.zip
