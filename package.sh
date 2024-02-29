aef0d1a70c57b4bbf27a64e6be439270e62f27ad
Cliff L. Biffle <code@cliffle.com>
2024-02-29 10:23:47 -0800

    Rearrange location of package.sh tmpfiles

--- package.sh
+++ package.sh
@@ -4,23 +4,27 @@ set -e -o pipefail
 
 BUILD="cargo build --release --no-default-features --features"
 OUT=target/thumbv6m-none-eabi/release
+REL=target/firmware-release
+
+mkdir -p $REL
 
 $BUILD stm32g030,panic-halt
-cp $OUT/keybad-fw $OUT/G03xxx_G04xxx.elf
-arm-none-eabi-objcopy -Obinary $OUT/keybad-fw $OUT/G03xxx_G04xxx.bin
+cp $OUT/keybad-fw $REL/G03xxx_G04xxx.elf
+arm-none-eabi-objcopy -Obinary $OUT/keybad-fw $REL/G03xxx_G04xxx.bin
 $BUILD stm32c011,panic-halt
-cp $OUT/keybad-fw $OUT/C011xx.elf
-arm-none-eabi-objcopy -Obinary $OUT/keybad-fw $OUT/C011xx.bin
-cp $OUT/C011xx.elf $OUT/C031xx.elf
-cp $OUT/C011xx.bin $OUT/C031xx.bin
+cp $OUT/keybad-fw $REL/C011xx.elf
+arm-none-eabi-objcopy -Obinary $OUT/keybad-fw $REL/C011xx.bin
+
+# Support STM32C031 should I have one in stock and install it -- from the
+# firmware's perspective it is a C011.
+cp $REL/C011xx.elf $REL/C031xx.elf
+cp $REL/C011xx.bin $REL/C031xx.bin
 
 VERSION=$(cargo metadata --format-version 1 \
     | jq -r '.resolve.root as $root | .packages[] | select(.id == $root) | .version')
-echo $VERSION > $OUT/VERSION
+echo $VERSION > $REL/VERSION
 
-cd $OUT
-zip firmware.zip VERSION \
-    C031xx.bin C011xx.bin G03xxx_G04xxx.bin \
-    C031xx.elf C011xx.elf G03xxx_G04xxx.elf
+cd $REL
+zip firmware.zip *
 cd -
-mv $OUT/firmware.zip keypad-go-firmware-$VERSION.zip
+mv $REL/firmware.zip keypad-go-firmware-$VERSION.zip
